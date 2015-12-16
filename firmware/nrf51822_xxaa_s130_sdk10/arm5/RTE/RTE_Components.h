f246b179dd009d4dc1eeac8313616233ee6c9280
Akihiro Uehara <u-akihiro@reinfoce-lab.com>
2015-12-16 20:08:05 +0900

    テンプレートプロジェクトを追加。

---
+++ firmware/nrf51822_xxaa_s130_sdk10/arm5/RTE/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_template_s130_pca10028' 
+ * Target:  'nrf51422_xxac_s130' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS
+#define GPIOTE_ENABLED
+  #define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS
+#define S130
+#define SOFTDEVICE_PRESENT
+#define SWI_DISABLE0
+#define UART0_ENABLED
+
+#endif /* RTE_COMPONENTS_H */

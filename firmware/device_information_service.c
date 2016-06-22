f65d02b35a43ffd86f0ee8a0b395c803537d130e
Akihiro Uehara <u-akihiro@reinfoce-lab.com>
2016-06-22 21:44:18 +0900

    ファームウェア、デバイスのシリアル番号をDeviceInformationServiceに追加。

--- firmware/device_information_service.c
+++ firmware/device_information_service.c
@@ -1,11 +1,15 @@
 #include <string.h>
+#include <stdio.h>
 
+#include <nrf51.h>
 #include <ble_dis.h>
 #include <app_error.h>
 #include <sdk_errors.h>
 
 #include "device_information_service.h"
 #include "senstick_device_definition.h"
+
+static char m_serial_string[17];
 
 // デバイスインフォアメーションサービスを初期化
 void init_device_information_service(void)
@@ -14,11 +18,16 @@ void init_device_information_service(void)
     ble_dis_init_t dis_init_obj;
     
     memset(&dis_init_obj, 0, sizeof(dis_init_obj));
+
+    // シリアル文字列を設定
+    memset(m_serial_string, 0, sizeof(m_serial_string));
+    sprintf(m_serial_string,"%08x%08x", NRF_FICR->DEVICEID[1], NRF_FICR->DEVICEID[0]);
     
     // 文字列を設定
     ble_srv_ascii_to_utf8(&dis_init_obj.manufact_name_str, MANUFACTURER_NAME);
     ble_srv_ascii_to_utf8(&dis_init_obj.hw_rev_str, HARDWARE_REVISION_STRING);
     ble_srv_ascii_to_utf8(&dis_init_obj.fw_rev_str, FIRMWARE_REVISION_STRING);
+    ble_srv_ascii_to_utf8(&dis_init_obj.serial_num_str, m_serial_string);
     
     // 属性設定
     //    BLE_GAP_CONN_SEC_MODE_SET_ENC_NO_MITM(&dis_init_obj.dis_attr_md.read_perm);

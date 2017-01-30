3659b45054fb3de5f5b7dfdde39106a38393c3d2
Akihiro Uehara <u-akihiro@reinfoce-lab.com>
2017-01-30 14:47:34 +0900

    ファームウェアのバイナリを作成。rev1.07

--- firmware/senstick_device_definition.h
+++ firmware/senstick_device_definition.h
@@ -20,7 +20,7 @@
 // ファームウェアは、機能が同じであるならば、同じ番号を用いる。
 // FIRMWARE_REVISIONは、ファームウェアのリビジョン。先頭1バイトがメジャーバージョン、後ろ1バイトがマイナーバージョン 0xJJMN の表記。
 // FIRMWARE_REVISION_STRINGは、ファームウェアのリビジョンを表す文字列。Device Information Serviceで使います
-#define	FIRMWARE_REVISION           0x0106
-#define FIRMWARE_REVISION_STRING    "rev 1.06"
+#define	FIRMWARE_REVISION           0x0107
+#define FIRMWARE_REVISION_STRING    "rev 1.07"
 
 #endif /* senstick_device_definition_h */

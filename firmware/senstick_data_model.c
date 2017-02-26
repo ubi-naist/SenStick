b2a42ef7278ae719e2308c6f54f609162efd4c52
Akihiro Uehara <u-akihiro@reinfoce-lab.com>
2017-02-26 20:46:05 +0900

    非接続時のスリープした時のアドバタイジング停止していないバグ修正。

--- firmware/senstick_data_model.c
+++ firmware/senstick_data_model.c
@@ -231,6 +231,7 @@ void senstick_setControlCommand(senstick_control_command_t command)
         case shouldDeviceSleep:
             // BLE接続していなければ、ここで電源を落とす。接続している場合は、切断完了時に電源を落とす。
             if( senstick_isConnected() == false) {
+                stopAdvertising();
                 twiPowerDown();
                 flashMemoryEnterDeepPowerDown();
             } else {
